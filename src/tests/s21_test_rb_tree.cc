0432f27e9cbd51feceab058d9c32b3ad4e77e2c1
Ildarc4S <ildarc4S@gmail.com>
2025-07-27 13:31:17 +0300

    testing clang-format-fix

--- src/tests/s21_test_rb_tree.cc
+++ src/tests/s21_test_rb_tree.cc
@@ -1,7 +1,5 @@
 #include <gtest/gtest.h>
+
 #include "../src/include/s21_rb_tree.h"
 
-TEST(RBTreeTest, ExampleTest) {
-  s21::RbTree alla(10);
-}
-
+TEST(RBTreeTest, ExampleTest) { s21::RbTree alla(10); }
