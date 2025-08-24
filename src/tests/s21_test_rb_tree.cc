21448dad3ae37a0e8f75946cfa635ea42b4a4acc
Ildarc4S <ildarc4S@gmail.com>
2025-08-24 18:53:38 +0300

    Add test creating RbTree object and insert

--- src/tests/s21_test_rb_tree.cc
+++ src/tests/s21_test_rb_tree.cc
@@ -1,7 +1,19 @@
 #include <gtest/gtest.h>
+#include <functional>
 
-#include "s21_rb_tree.h"
+#include "../include/s21_rb_tree.h"
+
+struct SelectFirst {
+  template<typename Pair>
+  constexpr const auto& operator()(const Pair& pair) const noexcept {
+    return pair.first;
+  }
+};
 
 TEST(RBTreeTest, ExampleTest) {
-  s21::RbTree r;
+  s21::RbTree<int, std::pair<const int, int>, SelectFirst, std::less<int>> tree1;
+
+  tree1.InsertUnique(std::pair<const int, int>(2, 2));
+  tree1.InsertUnique(std::pair<const int, int>(1, 2));
+  tree1.InsertUnique(std::pair<const int, int>(3, 2));
 }
