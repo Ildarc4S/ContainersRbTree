98e85dd0eddc42c45b7051d8d42281016c750dbb
Ildarc4S <ildarc4S@gmail.com>
2025-08-24 18:51:46 +0300

    Add test file for map

--- laboratory/map_test.cc
+++ laboratory/map_test.cc
@@ -1,6 +1,8 @@
 #include <map>
 
 int main() {
-  std::map<int, int>;
+  std::map<int, int> a;
+  a.insert(std::pair<const int, int>(2, 2));
+  a.insert(std::pair<const int, int>(1, 2));
   return 0;
 }
